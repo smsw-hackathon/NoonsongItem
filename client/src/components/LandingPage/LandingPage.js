700bb81fe000ba66bbea15ba514036cd1dfe465b
jeeewon <65210736+jeeewon@users.noreply.github.com>
2021-11-07 19:35:43 +0900

    LandPage

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Layout,Menu,Breadcrumb } from 'antd';
+import styled from 'styled-components';
+import axios from 'axios'
+const { Header, Footer, Content } = Layout;
 
-function Landingpage() {
-    return (
-        <div style={{
-            display: 'flex', justifyContent: 'center', alignItems: 'center',
-            width: '100%', height: '100vh'
-        }}>
-            <h2>시작 페이지</h2>
-        </div>
+const LandingPage = () =>{
+    return(
+        <Layout className="layout">
+            <StyledHeader>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['']}>
+                <Menu.Item>
+                    Home
+                </Menu.Item>
+                <Menu.Item>
+                    MyPage
+                </Menu.Item>
+                <Menu.Item>
+                    Create
+                </Menu.Item>
+            </Menu>
+            </StyledHeader>
+            <Content style={{ padding: '0 50px' }}>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Ant Design ?2018 Created by Ant UED</Footer>
+        </Layout>
     )
 }
 
-export default Landingpage
+const StyledHeader = styled(Header)`
+    color: #012F48;
+`
+
+export default LandingPage
